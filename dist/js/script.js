543a4237338708ac381069646c49c543abaf7308
Artem <example@gmail.com>
2020-12-01 14:45:00 +0500

    home

--- dist/js/script.js
+++ dist/js/script.js
@@ -37,5 +37,11 @@ window.addEventListener('DOMContentLoaded', () => {
               });
           }
     });
+
+    $("a[href^='#']").click(function(){
+          const _href = $(this).attr("href");
+          $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+          return false;
+        });
 });
 
