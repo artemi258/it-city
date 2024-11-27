3dc0e1042e6597d70dab1ba87f65afa225585cc5
It city <It@mail.ru>
2024-11-27 14:37:30 +0500

    доработка поиска картинок

--- utils/getImage.ts
+++ utils/getImage.ts
@@ -16,56 +16,36 @@ interface IImages {
 export const getImage = async (products: IProduct[]): Promise<IImages[]> => {
  const images = [];
 
- let browser: Browser;
-
- const openBrowser = async () => {
-  browser = await puppeteer.launch({
+ const getUrlImage = async (product: IProduct): Promise<string> => {
+  const browser = await puppeteer.launch({
    args: ['--disable-gpu', '--no-sandbox'],
    executablePath: '../../../usr/bin/chromium-browser',
   });
 
-  return browser;
- };
+  try {
+   const page = await browser.newPage();
+   console.log('product', product.name);
+   await page.goto('https://www.google.ru/imghp', { waitUntil: 'load' });
 
- await openBrowser();
+   await page.$eval('textarea', (input, localValue) => (input.value = localValue), product.name);
 
- const getUrlImage = (product: IProduct): Promise<string> => {
-  return new Promise((res, rej) => {
-   (async function async() {
-    const page = await browser.newPage().catch((err) =>
-     rej({
-      err: 'page',
-      message: `не удалось открыть страницу ${err}`,
-     }),
-    );
-    try {
-     if (page) {
-      console.log('product', product.name);
-      await page.goto('https://www.google.ru/imghp', { waitUntil: 'load' });
+   await new Promise((res) => setTimeout(res, 1000));
 
-      await page.$eval('textarea', (input, localValue) => (input.value = localValue), product.name);
+   await page.locator('[aria-label="Поиск в Google"]').click();
 
-      await new Promise((res) => setTimeout(res, 1000));
+   await page.waitForNavigation();
 
-      await page.locator('[aria-label="Поиск в Google"]').click();
+   await new Promise((res) => setTimeout(res, 1000));
 
-      await page.waitForNavigation();
+   const img = await page.$('.dURPMd img');
 
-      await new Promise((res) => setTimeout(res, 1000));
+   const url = await (await img.getProperty('src')).jsonValue();
+   await browser.close();
 
-      const img = await page.$('.dURPMd img');
-
-      const url = await (await img.getProperty('src')).jsonValue();
-      await page.close();
-
-      res(url);
-     }
-    } catch (error) {
-     if (page) await page.close();
-     rej(error);
-    }
-   })();
-  });
+   return url;
+  } catch (error) {
+   await browser.close();
+  }
  };
 
  for (let i = 0; i < products.length; i++) {
@@ -76,15 +56,9 @@ export const getImage = async (products: IProduct[]): Promise<IImages[]> => {
      name: products[i].name,
     }),
    )
-   .catch(async (err) => {
-    if (err.err) {
-     console.log(err.message);
-     await browser.close();
-     await openBrowser();
-    }
+   .catch((err) => {
     console.log(err);
    });
  }
- await browser.close();
  return images;
 };
